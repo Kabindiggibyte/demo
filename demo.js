aed3e9e80a70c9b1649589b6b52be2519b93695e
Kabindiggibyte <120174836+Kabindiggibyte@users.noreply.github.com>
2022-12-16 12:32:39 +0530

    demo

---
+++ demo.js
@@ -0,0 +1,17 @@
+//for in loop
+
+const student={name:"kavin",age:23};
+
+for(let key in student){
+    console.log(student.k);
+}
+
+//for of loop
+
+const stu=["anu","kavin"];
+
+for(let key of stu){
+    console.log(key);
+}
+
+
